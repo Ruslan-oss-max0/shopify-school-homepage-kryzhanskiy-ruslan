5de312f132556dbd6b7086c4bda799e23df6f447
Ruslan <kryzhanskiyruslan@gmail.com>
2025-09-05 05:41:07 +0300

    add the header, sidebar and the hero content

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-import './styles/styles.scss';
+import Swiper from "swiper";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./styles/styles.scss";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Pagination],
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
