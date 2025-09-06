180be0c21fd4370206f9388cd6b2c451a8fad4bf
Ruslan <kryzhanskiyruslan@gmail.com>
2025-09-06 23:36:22 +0300

    add the featured collection section

--- src/main.js
+++ src/main.js
@@ -1,15 +1,41 @@
 import Swiper from "swiper";
-import { Pagination } from "swiper/modules";
+import { Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 import "./styles/styles.scss";
 
-const swiper = new Swiper(".swiper", {
+const heroSlider = new Swiper(".hero__slider", {
   modules: [Pagination],
+  loop: true,
+
   pagination: {
-    el: ".swiper-pagination",
+    el: ".hero__slider-pagination",
     clickable: true,
   },
 });
 
+const futureCollectionSlider = new Swiper(".featured-collection__slider", {
+  modules: [Navigation],
+  slidesPerView: 1.1,
+  spaceBetween: 16,
+
+  navigation: {
+    nextEl: ".featured-collection__slider-button--next",
+    prevEl: ".featured-collection__slider-button--prev",
+    enabled: false,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 24,
+      navigation: { enabled: true },
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+      navigation: { enabled: true },
+    },
+  },
+});
